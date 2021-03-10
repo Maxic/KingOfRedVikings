777bfbcd667a8df55767013ccf63cb010de96a35
Maxic <barend.poot@gmail.com>
2021-03-10 16:33:50 +0100

    Added damping to shallow sea

---
+++ courseArea.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+var old_damp
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_Area2D_body_entered(body):
+	if body.name == 'body':
+		old_damp = body.linear_damp
+		body.linear_damp = 5
+
+func _on_Area2D_body_exited(body):
+	if body.name == 'body':
+		body.linear_damp = old_damp
