4909eb60b0b32cd84a356537bdd8794cec5611af
Maxic <barend.poot@gmail.com>
2021-03-10 10:03:15 +0100

    Implemented zoom and offset in camera

--- Camera.gd
+++ Camera.gd
@@ -17,26 +17,18 @@ func _process(delta):
 	var true_velocity = sqrt(pow(linear_velocity.x,2)+pow(linear_velocity.y,2)) *.15
 	var old_offset = self.offset
 	var old_zoom = self.zoom
-	var max_zoom
+	var max_zoom = 1.5
 
 	# Zoom out when going fast
+	self.zoom = lerp(self.zoom, (self.zoom*(true_velocity/39)+Vector2(.6,.6)), delta*10)
 
-	self.zoom = lerp(self.zoom, self.zoom*((true_velocity*.1)-2), delta)
-	print(true_velocity)
-	#print("want to: " + str(zoom))
-
-	if true_velocity < 20:
-		max_zoom = 1.5
-	else:
-		max_zoom = 2
-	if abs(self.zoom.y) > max_zoom:
+	if self.zoom > old_zoom and self.zoom.y > max_zoom:
 		self.zoom = old_zoom
 	if self.zoom.y < 1:
 		self.zoom = old_zoom	
-	#print("actual: " + str(zoom))
-	# Change offset according to speed
 
-	self.offset = lerp(self.offset, linear_velocity, delta)
+	# Change offset according to speed
+	self.offset = lerp(self.offset, linear_velocity, delta*2)
 	if abs(self.offset.y) > x_margin:
 		self.offset.y = old_offset.y	
 	if abs(self.offset.x) > y_margin:
