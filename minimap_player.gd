b37648481fdf3e9dd03a0412d80209f9b7b34be1
David <david.alpha@gmail.com>
2021-03-12 17:40:01 +0100

    minimap fixed

--- minimap_player.gd
+++ minimap_player.gd
@@ -4,7 +4,7 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var offset_position = Vector2(202, 20.5)
+var offset_position = Vector2(192, 28)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +21,6 @@ func _process(delta):
 	var player_rotation = player.rotation
 	#print(player_postion.y)
 	self.rotation = player_rotation
-	self.position.x = (player_position.x * 0.01) + offset_position.x
-	self.position.y = (player_position.y * 0.01) + offset_position.y
+	self.position.x = (player_position.x * 0.012) + offset_position.x
+	self.position.y = (player_position.y * 0.012) + offset_position.y
 	
