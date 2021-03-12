19de311ee9a619ceb88f7b40f2ac54c75a7639ec
David <david.alpha@gmail.com>
2021-03-12 13:40:15 +0100

    minimap moved

--- minimap_player.gd
+++ minimap_player.gd
@@ -4,7 +4,7 @@ extends Sprite
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var offset_position = Vector2(3.5, 16.5)
+var offset_position = Vector2(200.5, 20.5)
 
 
 # Called when the node enters the scene tree for the first time.
